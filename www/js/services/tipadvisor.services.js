421cc23ef186b9e75da5c2110a306ae4d8938c60
John Keith <johnkeith@outlook.com>
2014-09-09 19:15:04 -0400

    all countries from mint infographic

--- www/js/services/tipadvisor.services.js
+++ www/js/services/tipadvisor.services.js
@@ -27,9 +27,20 @@ angular.module("tipadvisor.services", [])
       { id: 0, code: "us", name: "United States", avgTip: "15 - 20%", notes: "All service industries expect tipping." },
       { id: 1, code: "ca", name: "Canada", avgTip: "15 - 20%", notes: "All service industries expect tipping." },
       { id: 2, code: "gb", name: "United Kingdom", avgTip: "10 - 15%", notes: "Often service charges are included (look for an 'optional' charge on the bill). No tipping necessary at bars and pubs." },
-      { id: 3, code: "ch", name: "Switzerland", avgTip: "15%", notes: "Almost all services include charge on bill, but a small tip at upscale establishments is acceptable and appreciated." },
+      { id: 3, code: "ch", name: "Switzerland", avgTip: "15%", notes: "Almost all services include a charge on bill, but a small tip at upscale establishments is acceptable and appreciated." },
       { id: 4, code: "de", name: "Germany", avgTip: "10 - 15%", notes: "Service at a restaurant usually necessitates tipping." },
-      { id: 5, code: "tr", name: "Turkey", avgTip: "10%", notes: "Tipping in restaurants is customary, though only cash is accepted." }
+      { id: 5, code: "tr", name: "Turkey", avgTip: "10%", notes: "Tipping in restaurants is customary, though only cash is accepted." },  
+      { id: 6, code: "it", name: "Italy", avgTip: "10%", notes: "No more than 10% tip is required. No need, however, to tip gondoliers when riding the canals of Venice." },
+      { id: 7, code: "cn", name: "China", avgTip: "0%", notes: "Tipping is not customary in China." },
+      { id: 8, code: "jp", name: "Japan", avgTip: "0%", notes: "Tipping is not customary in Japan." },
+      { id: 9, code: "kr", name: "South Korea", avgTip: "0%", notes: "Tipping is not customary in South Korea." },
+      { id: 10, code: "au", name: "Australia", avgTip: "10 - 15%", notes: "While tipping was not always a common practice, now a small tip for good service is the general rule." },
+      { id: 11, code: "in", name: "India", avgTip: "15%", notes: "Often, a 10% service charge will be included, but be sure to leave a tip if that is not the case." },
+      { id: 12, code: "eg", name: "Egypt", avgTip: "5 - 10%", notes: "Service professional appreciate a small tip added to the charge already included in the bill." },
+      { id: 13, code: "br", name: "Brazil", avgTip: "0%", notes: "Tipping is not customary in Brazil, though restaurants often charge a 10% service fee." },
+      { id: 14, code: "fr", name: "France", avgTip: "10%", notes: "Most services include a tip in the bill and tourists are not expected to tip, though locals often leave an extra 10% at restaurants." },
+      { id: 15, code: "ar", name: "Argentina", avgTip: "10%", notes: "Tipping is generally not customary, though it is recommended in upscale restaurants." },
+      { id: 16, code: "mx", name: "Mexico", avgTip: "10 - 15%", notes: "Tipping is expected and dollars are accepted." }
     ];
 
     return {
