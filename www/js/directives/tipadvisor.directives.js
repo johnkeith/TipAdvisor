da73f7c157d6a0245295115320f2f348694c6b05
John Keith <johnkeith@outlook.com>
2014-09-10 17:27:58 -0400

    basic slide up animation on app load with calc

--- www/js/directives/tipadvisor.directives.js
+++ www/js/directives/tipadvisor.directives.js
@@ -1,6 +1,10 @@
 angular.module('tipadvisor.directives', [])
-  .directive('calc', function(){
-    return {
-      templateUrl: './templates/calc.html'
-    }
-  });
+  .directive('calc', ["$animate",
+    function($animate){
+      return {
+        templateUrl: './templates/calc.html',
+        link: function(scope, element, attrs){
+          $animate.addClass(element, 'slide-up-calc');
+        }
+      }
+  }]);
