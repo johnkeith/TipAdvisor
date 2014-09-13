21448f23412c48dc1571b8c7ff2a174059455539
John Keith <johnkeith@outlook.com>
2014-09-12 21:17:57 -0400

    directive for showing calc working on settings page

--- www/js/directives/tipadvisor.directives.js
+++ www/js/directives/tipadvisor.directives.js
@@ -7,23 +7,40 @@ angular.module('tipadvisor.directives', [])
           $animate.addClass(element, 'slide-up-calc');
         }
       };
-  }])
-  .directive('showCalc', ["$compile",
+  }]);
+angular.module('tipadvisor.directives')
+  .directive('showcalc', ["$compile",
     function($compile){
       return function(scope, element, attrs){
         element.bind("click", function(){
           console.log("I'm in the directive");
-          // var calcSpace = angular.element(document.getElementById('calc-space'));
-          // var ionContent = angular.element(document.getElementById('calc-showable'));
-          // if (calcSpace == null){
-          //   ionContent.append($compile("<div calc></div>")(scope));
-          // }
-          // else {
-          //   calcSpace.remove();
-          // }
-        });
-      };
+          var calcSpace = angular.element(document.getElementById('calc'));
+          var ionContent = angular.element(document.getElementById('calc-showable'));
+          if (calcSpace.length == 0){
+            ionContent.append($compile("<div calc class='hidden-calc' id='calc'></div>")(scope));  
+          }
+          else {
+            angular.element(document.getElementById('calc')).remove();          
+          }
+        })
+      }
   }]);
+  // .directive('showCalc', ["$compile",
+  //   function($compile){
+  //     return function(scope, element, attrs){
+  //       element.bind("click", function(){
+  //         console.log("I'm in the directive");
+  //         var calcSpace = angular.element(document.getElementById('calc-space'));
+  //         var ionContent = angular.element(document.getElementById('calc-showable'));
+  //         if (calcSpace == null){
+  //           ionContent.append($compile("<div calc></div>")(scope));
+  //         }
+  //         else {
+  //           calcSpace.remove();
+  //         }
+  //       });
+  //     };
+  // }]);
   // .directive('showCalc', ["$compile",
   //   function(){
   //     console.log("I'm in the func!");
