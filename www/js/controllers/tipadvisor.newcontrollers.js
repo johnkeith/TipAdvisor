ddfb40a4016288a735869892fd93caee0afbcbbe
John Keith <johnkeith@outlook.com>
2014-09-19 23:12:37 -0400

    tax recalcing correctly via currency input and slider change of percent

--- www/js/controllers/tipadvisor.newcontrollers.js
+++ www/js/controllers/tipadvisor.newcontrollers.js
@@ -4,4 +4,25 @@ angular.module('tipadvisor.newcontrollers', [])
       $scope.bill = billFactory.getBill();
       $scope.tip = tipFactory.getTip();
       $scope.tax = taxFactory.getTax();
+
+      $scope.calcTaxCur = function(){
+        taxFactory.setCurrencyFromPercent(billFactory.getBillFloat());
+        console.log(taxFactory.getTax());
+      };
+      // YOU HAVE TO USE A DOT! OTHERWISE THE WATCH DOESN'T WORK
+      // $scope.$watch('bill.dollars', function(n, o){
+      //   console.log("watch firing");
+      //   taxFactory.setCurrencyFromPercent(billFactory.getBillFloat());
+      // });
+
+      $scope.recalcTaxCurrency = function(){
+        taxFactory.setCurrencyFromPercent(billFactory.getBillFloat());
+      };
+      $scope.recalcTaxPercent = function(){
+        taxFactory.setPercentFromCurrency(billFactory.getBillFloat());
+      };
+
+      // instead, place a watch on bill, tip, and tax
+      // when those change, fire recalcs
+      // define calcs in service for calculations
   }]);
--- www/js/services/tipadvisor.newfactories.js
+++ www/js/services/tipadvisor.newfactories.js
@@ -1,17 +1,20 @@
 angular.module('tipadvisor.newfactories', [])
   .factory('billFactory', function(){
     var bill = {
-      dollars: "",
-      cents: ""
+      dollars: "0",
+      cents: "0"
     };
 
     return {
       getBill: function(){
         return bill;
       },
+      getBillFloat: function(){
+        return parseFloat(bill.dollars + "." + bill.cents);
+      },
       setBillDollars: function(d){
         bill.dollars = d;
-      }, 
+      },
       setBillCents: function(c){
         bill.cents = c;
       }
@@ -19,7 +22,8 @@ angular.module('tipadvisor.newfactories', [])
   })
   .factory('tipFactory', function(){
     var tip = {
-      percent: "0"
+      percent: "0",
+      currency: "0"
     };
 
     return {
@@ -28,13 +32,16 @@ angular.module('tipadvisor.newfactories', [])
       },
       setTip: function(p){
         tip.percent = p;
+      },
+      setCurrency: function(c){
+        tip.currency = c;
       }
-    }
-
+    };
   })
   .factory('taxFactory', function(){
     var tax = {
-      percent: "0"
+      percent: "0",
+      currency: "0"
     };
 
     return {
@@ -43,6 +50,17 @@ angular.module('tipadvisor.newfactories', [])
       },
       setTip: function(p){
         tax.percent = p
+      },
+      setCurrency: function(c){
+        tax.currency = c;
+      }, 
+      setCurrencyFromPercent: function(bill){
+        console.log(bill);
+        console.log(tax.percent);
+        tax.currency = bill * tax.percent;
+      },
+      setPercentFromCurrency: function(bill){
+        tax.percent = tax.currency / bill;
       }
-    }
+    };
   });
